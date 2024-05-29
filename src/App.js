b06d5ea159f057b5988efdaa821e4cc65e09f27d
Kavya Sree <94122001+kavya-sree1@users.noreply.github.com>
2024-05-29 13:54:15 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Calculator from './Calculator.js';
+import AppBar from './AppBar';
+
+function App() {
+   return(
+    <>
+    <AppBar/>
+    <Calculator/>
+    </>
+  ) 
+}
+
+export default App;
